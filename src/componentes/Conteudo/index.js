3826796dcb590c6eb08e490c1d3d0344528bd102
André Ferrari <andrhenrique6488@gmail.com>
2022-08-05 13:51:04 -0300

    Finalizando a estilização da home page

--- src/componentes/Conteudo/index.js
+++ src/componentes/Conteudo/index.js
@@ -14,9 +14,9 @@ function Conteudo() {
 
       <section className='sessao sabores'>
         <img src={sabores} />
-        <div>
+        <div className='sessao_texto'>
           <h2>NOSSOS SABORES</h2>
-          <p>Novos e deliciosos!</p>
+          <span>Novos e deliciosos!</span>
           <p>
             Sorvete bom é aquele feito com os melhores ingredientes! Aqui na gelateria todos os nossos produtos são naturais, à base de frutas e sem nenhum convervante! Também temos opções sem lactose e sem açúcar. Venha conhecer e perceber que tem como o sorvete ser delicioso e saudável ao mesmo tempo!
           </p>
@@ -24,9 +24,9 @@ function Conteudo() {
       </section>
 
       <section className='sessao eventos'>
-      <div>
+      <div className='sessao_texto'>
           <h2>NOSSOS EVENTOS</h2>
-          <p>Delícias com sorvete!</p>
+          <span>Delícias com sorvete!</span>
           <p>
             Mais do que uma sorveteria, uma extenção da sua casa! Estamos aqui prontinhos para te atender e oferecer os melhores eventos com os melhores sorvetes da sua vida! Venha nos conhecer e passar um tempo aqui com a gente
           </p>
@@ -36,9 +36,9 @@ function Conteudo() {
 
       <section className='sessao sobre'>
         <img src={sobre} />
-        <div>
+        <div className='sessao_texto'>
         <h2>SOBRE NÓS</h2>
-          <p>Alegria em cada casquinha!</p>
+          <span>Alegria em cada casquinha!</span>
           <p>
             Venha tomar o melhor sorvete da região aqui com a gente! Nós estamos há anos no mercado produzindo o que tem de melgor para o nosso cliente e você não pode ficar dora dessa. Venha nos fazer uma visita e aproveite o melhor atendimento e o melhor sorvete da cidade.
           </p>
--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import './style.css'
+import logo from '../../assets/logo.png'
 
 function Rodape() {
   return (
     <footer id='home_rodape'>
 
-      <div>
-        <img/>
-        <div>
+      <div className='limitar_sessao'>
+        <img src={logo}/>
+        <div className='elemento_rodape'>
           <h2>ENDEREÇO</h2>
           <p>Av. Bernardino de Campos, 98</p>
           <p>São Paulo, SP 12345-678</p>
         </div>
-        <div>
+        <div className='elemento_rodape'>
           <h2>CONTATO</h2>
           <p>info@meusite.com</p>
           <p>Tel: (11) 3456-7890</p>
         </div>
-        <div>
+        <div className='elemento_rodape'>
           <h2>HORÁRIOS</h2>
           <p>ABERTO TODOS OS DIAS</p>
           <p>10:00 - 18:00</p>
         </div>
       </div>
 
-      <div>
+      <div className='elemento_rodape_final'>
         <p>Gelateria. Orgulhosamente desenvolvido por 'André Ferrari'</p>
       </div>
 
